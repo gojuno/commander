8c603acf15123b2afc4696df961e45e4b71b35ec
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-04-13 22:29:35 +0300

    Extract Commander from Composer into separate repo.

---
+++ README.md
@@ -0,0 +1,48 @@
+## Commander — set of reactive functions for cli tools.
+
+Commander provides functions to work with processes, files and other things useful for cli tools like [Swarmer](https://github.com/gojuno/swarmer) and [Composer](https://github.com/gojuno/composer).
+
+Modules:
+
+* [os](os/) — functions to work with processes and files on different Operating Systems.
+* [android](android/) — functions to work with Android SDK Tools like adb, avdmanager, sdkmanager.
+
+### Download
+
+Commander is [available on jcenter](https://jcenter.bintray.com/com/gojuno/commander).
+
+```groovy
+compile 'com.gojuno.commander:os:some-version'
+compile 'com.gojuno.commander:android:some-version'
+```
+
+All the releases and changelogs can be found on [Releases Page](https://github.com/gojuno/commander/releases).
+
+### How to build
+
+Dependencies: you only need `docker` and `bash` installed on your machine.
+
+```console
+bash ci/build.sh
+```
+
+## License
+
+```
+Copyright 2017 Juno, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+[spoon]: https://github.com/square/spoon
+[test sharding]: https://developer.android.com/topic/libraries/testing-support-library/index.html#ajur-sharding
---
+++ android/src/main/kotlin/com/gojuno/commander/android/Adb.kt
@@ -0,0 +1,122 @@
+package com.gojuno.commander.android
+
+import com.gojuno.cmd.common.Notification
+import com.gojuno.cmd.common.log
+import com.gojuno.cmd.common.nanosToHumanReadableTime
+import com.gojuno.cmd.common.process
+import rx.Observable
+import rx.Single
+import java.io.File
+import java.util.concurrent.TimeUnit.MINUTES
+import java.util.concurrent.TimeUnit.SECONDS
+
+val androidHome: String by lazy { System.getenv("ANDROID_HOME") }
+val adb: String by lazy { "$androidHome/platform-tools/adb" }
+
+fun connectedAdbDevices(): Observable<Set<AdbDevice>> = process(listOf(adb, "devices"), unbufferedOutput = true)
+        .ofType(Notification.Exit::class.java)
+        .map { it.output.readText() }
+        .map {
+            when (it.contains("List of devices attached")) {
+                true -> it
+                false -> throw IllegalStateException("Adb output is not correct: $it.")
+            }
+        }
+        .retry { retryCount, exception ->
+            val shouldRetry = retryCount < 5 && exception is IllegalStateException
+            if (shouldRetry) {
+                log("runningEmulators: retrying $exception.")
+            }
+
+            shouldRetry
+        }
+        .map {
+            it
+                    .substringAfter("List of devices attached")
+                    .split(System.lineSeparator())
+                    .map { it.trim() }
+                    .filter { it.isNotEmpty() }
+                    .filter { it.contains("online") || it.contains("device") }
+                    .map {
+                        AdbDevice(
+                                id = it.substringBefore("\t"),
+                                online = when {
+                                    it.contains("offline", ignoreCase = true) -> false
+                                    it.contains("device", ignoreCase = true) -> true
+                                    else -> throw IllegalStateException("Unknown adb output for device: $it")
+                                }
+                        )
+                    }
+                    .toSet()
+        }
+        .doOnError { log("Error during getting connectedAdbDevices, error = $it") }
+
+fun AdbDevice.log(message: String) = com.gojuno.cmd.common.log("[$id] $message")
+
+fun AdbDevice.installApk(pathToApk: String): Observable<Unit> {
+    val adbDevice = this
+    val installApk = process(
+            commandAndArgs = listOf(adb, "-s", adbDevice.id, "install", "-r", pathToApk),
+            unbufferedOutput = true,
+            timeout = 5 to MINUTES
+    )
+
+    return Observable
+            .fromCallable { System.nanoTime() }
+            .flatMap { startTimeNanos -> installApk.ofType(Notification.Exit::class.java).map { it to startTimeNanos } }
+            .map { (exit, startTimeNanos) ->
+                val success = exit
+                        .output
+                        .readText()
+                        .split(System.lineSeparator())
+                        .map { it.trim() }
+                        .filter { it.isNotEmpty() }
+                        .firstOrNull { it.equals("Success", ignoreCase = true) } != null
+
+                val duration = System.nanoTime() - startTimeNanos
+
+                when (success) {
+                    true -> {
+                        adbDevice.log("Successfully installed apk in ${duration.nanosToHumanReadableTime()}, pathToApk = $pathToApk")
+                    }
+
+                    false -> {
+                        adbDevice.log("Failed to install apk $pathToApk")
+                        System.exit(1)
+                    }
+                }
+            }
+            .doOnSubscribe { adbDevice.log("Installing apk... pathToApk = $pathToApk") }
+            .doOnError { adbDevice.log("Error during installing apk: $it, pathToApk = $pathToApk") }
+}
+
+fun AdbDevice.pullFolder(folderOnDevice: String, folderOnHostMachine: File, logErrors: Boolean): Single<Boolean> {
+    val adbDevice = this
+    val pullFiles = process(
+            commandAndArgs = listOf(adb, "-s", adbDevice.id, "pull", folderOnDevice, folderOnHostMachine.absolutePath),
+            timeout = 60 to SECONDS,
+            unbufferedOutput = true
+    )
+
+    return pullFiles
+            .ofType(Notification.Exit::class.java)
+            .retry(3)
+            .doOnError { error -> if (logErrors) log("Failed to pull files from $folderOnDevice to $folderOnHostMachine failed: $error") }
+            .map { true }
+            .onErrorReturn { false }
+            .toSingle()
+}
+
+fun AdbDevice.redirectLogcatToFile(file: File): Single<Process> = Observable
+        .fromCallable { file.parentFile.mkdirs() }
+        .flatMap { process(listOf(adb, "-s", this.id, "logcat"), redirectOutputTo = file, timeout = null) }
+        .ofType(Notification.Start::class.java)
+        .doOnError {
+            when (it) {
+                is InterruptedException -> Unit // Expected case, interrupt comes from System.exit(0).
+                else -> this.log("Error during redirecting logcat to file $file, error = $it")
+            }
+        }
+        .map { it.process }
+        .take(1)
+        .toSingle()
