1a4de5ad9185924deea8a7e1b93c807c58628132
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-02-01 03:01:18 +0100

    Add device model name support for https://github.com/gojuno/composer/issues/122.

--- android/src/main/kotlin/com/gojuno/commander/android/Emulator.kt
+++ android/src/main/kotlin/com/gojuno/commander/android/Emulator.kt
@@ -2,6 +2,7 @@ package com.gojuno.commander.android
 
 data class AdbDevice(
         val id: String,
+        val model: String,
         val online: Boolean
 ) {
     val isEmulator = id.startsWith("emulator-")
